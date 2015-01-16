73992f5d637cf169b4880641375bf41f00eafb5c
samutamm <samutamm@gmail.com>
2015-01-16 10:30:27 +0200

    Tests for method paaluokka.take-until

--- matrix-calculator/test/matrix_calculator/paaluokka_test.clj
+++ matrix-calculator/test/matrix_calculator/paaluokka_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [matrix_calculator.paaluokka :refer :all]))
 
-(deftest paaluokka.take-until
+(deftest paaluokka.take-until.zero
   (testing "With parameter zero, returnvalue contains one empty element."
-    (is (take-until 0 []))))
+    (is (= (take-until 0 []) [[][]]))
+    (is (= (take-until 0 [1 2 3]) [[][1 2 3]]))))
+
+(deftest paaluokka.take-until.smaller
+  (testing "With parameter smaller than the size of the given sequence,
+    returnvalue contains two nonempty arrays."
+    (is (= (take-until 1 [1 2]) [[1] [2]]))
+    (is (= (take-until 3 [55 22 33 44 55]) [[55 22 33] [44 55]]))))
+
+(deftest paaluokka.take-until.bigger
+  (testing "With parameter bigger than the size of the given sequence,
+    returnvalue contains two arrays with some nil values"
+    (is (= (take-until 2 []) [[nil nil][]]))
+    (is (= (take-until 4 [1 2 3]) [[1 2 3 nil][]]))))
