8dabe9c8289b0a7e0365dfd8df32a1307aa50351
samutamm <samutamm@gmail.com>
2015-01-16 11:27:00 +0200

    Test for make-matrix method.

--- matrix-calculator/test/matrix_calculator/paaluokka_test.clj
+++ matrix-calculator/test/matrix_calculator/paaluokka_test.clj
@@ -18,3 +18,16 @@
     returnvalue contains two arrays with some nil values"
     (is (= (take-until 2 []) [[nil nil][]]))
     (is (= (take-until 4 [1 2 3]) [[1 2 3 nil][]]))))
+
+(deftest paaluokka.make-matrix.correct-input
+  (testing "With correct parameters, the method returns a reprensation of matrix."
+    (is (= (make-matrix 1 1 1) [[1]]))
+    (is (= (make-matrix 1 3 1 2 3) [[1 2 3]]))
+    (is (= (make-matrix 3 1 4 3 2) [[4][3][2]] ))
+    (is (= (make-matrix 3 3 1 2 3 4 5 6 7 8 9) [[1 2 3][4 5 6][7 8 9]]))))
+
+(deftest paaluokka.make-matrix.incorrect-input
+  (testing "When first and second parameter are multipled and the result is
+    not same as the number of the rest parameters, exeption should be thrown."
+    (is (thrown? Exception (make-matrix 1 1 1 2)))
+    (is (thrown? Exception (make-matrix 2 2 1 2 3)))))
