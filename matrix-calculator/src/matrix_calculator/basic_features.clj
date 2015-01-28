c762dc537f78ceb616847af249f8ea87e5f5a1fb
samutamm <samutamm@gmail.com>
2015-01-28 17:32:21 +0200

    Strassen is allmost(?) working :). Need to debug a bit

--- matrix-calculator/src/matrix_calculator/basic_features.clj
+++ matrix-calculator/src/matrix_calculator/basic_features.clj
@@ -1,18 +1,27 @@
 (ns matrix_calculator.basic_features
   (:use matrix_calculator.helpers))
 
-(defn add-two-matrices
-  "Takes two matrices as parameters. If the matrices are same type, sums them:
-  To say, add every element of matrix-a to an according one of b. Returns an new matrix of same type.
-  (A + B)(i,j) = A(i, j) + B(i, j) for all i to m and j to n, where m*n is the size/type of the matrices."
-  [matrix-a matrix-b]
+(defn each-elem-in-matrices
+  [matrix-a matrix-b function]
     (if
       (or
        (not= (count matrix-a) (count matrix-b))
        (not= (count (first matrix-a)) (count (first matrix-b))))
         (matrices-are-not-valid-type)
-        (let [add-row (fn [row-a row-b] (map (fn [x y] (+ x y)) row-a row-b))]
+        (let [add-row (fn [row-a row-b] (map function row-a row-b))]
            (map add-row matrix-a matrix-b))))
+
+(defn add-two-matrices
+  "Takes two matrices as parameters. If the matrices are same type, sums them:
+  To say, add every element of matrix-a to an according one of b. Returns an new matrix of same type.
+  (A + B)(i,j) = A(i, j) + B(i, j) for all i to m and j to n, where m*n is the size/type of the matrices."
+  [matrix-a matrix-b]
+    (each-elem-in-matrices matrix-a matrix-b (fn [x y] (+ x y))))
+; HUOM alla olevaan testit ja dokumentaatio kommentti
+
+(defn subtract-two-matrices
+    [matrix-a matrix-b]
+    (each-elem-in-matrices matrix-a matrix-b (fn [x y] (- x y))))
 
 (defn scalar-product
   "Takes one number and one matrix as parameters. Multiples every element of matrix by the scalar.
--- matrix-calculator/src/matrix_calculator/multiply.clj
+++ matrix-calculator/src/matrix_calculator/multiply.clj
@@ -43,11 +43,53 @@
          (vec (take how-long (drop fromX row))))
        (take how-long (drop fromY matrix)))))
 
+(defn concat-matrices
+  [m11 m21 m12 m22]
+  (let [concat-two (fn [upper lower] (vec (concat upper lower)))]
+    (vec (map (fn [left right]
+           (vec (concat left right)))
+         (concat-two m11 m21)
+         (concat-two m12 m22)))))
+
 (defn strassen
   [matrixA matrixB]
   (let [n (count matrixA)]
-    (if (= n 1)
+    (if (= n 2)
       (matrix-multiplication matrixA matrixB)
       (let [new-size (/ n 2)
-            a11 (make-empty-matrix new-size new-size)]))
-    "kesken"))
+            a11 (sub-matrix matrixA 0 0 new-size)
+            a12 (sub-matrix matrixA new-size 0 new-size)
+            a21 (sub-matrix matrixA 0 new-size new-size)
+            a22 (sub-matrix matrixA new-size new-size new-size)
+            b11 (sub-matrix matrixB 0 0 new-size)
+            b12 (sub-matrix matrixB new-size 0 new-size)
+            b21 (sub-matrix matrixB 0 new-size new-size)
+            b22 (sub-matrix matrixB new-size new-size new-size)
+
+            a-result (add-two-matrices a11 a22)
+            b-result (add-two-matrices b11 b22)
+            p1 (strassen a-result b-result)
+            a-result (add-two-matrices a21 a22)
+            p2 (strassen a-result b11)
+            b-result (subtract-two-matrices b12 b22)
+            p3 (strassen a11 b-result)
+            b-result (subtract-two-matrices b21 b11)
+            p4 (strassen a22 b-result)
+            a-result (add-two-matrices a11 a12)
+            p5 (strassen a-result b22)
+            a-result (subtract-two-matrices a21 a11)
+            b-result (add-two-matrices b11 b12)
+            p6 (strassen a-result b-result)
+            a-result (subtract-two-matrices a12 a22)
+            b-result (add-two-matrices b21 b22)
+            p7 (strassen a-result b-result)
+
+            c12 (add-two-matrices p3 p5)
+            c21 (add-two-matrices p2 p4)
+            a-result (add-two-matrices p1 p4)
+            b-result (add-two-matrices a-result p7)
+            c11 (subtract-two-matrices b-result p5)
+            a-result (add-two-matrices p1 p3)
+            b-result (add-two-matrices a-result p6)
+            c22 (subtract-two-matrices b-result p2)]
+        (concat-matrices c11 c21 c12 c22)))))
