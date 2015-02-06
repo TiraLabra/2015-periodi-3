2938ac77b4eec271e3376887d14b4cff0cdb642f
samutamm <samutamm@gmail.com>
2015-02-06 17:16:06 +0200

    Start determinant-function

---
+++ matrix-calculator/src/matrix_calculator/determinant.clj
@@ -0,0 +1,6 @@
+(ns matrix_calculator.determinant
+  (:use matrix_calculator.helpers)
+  (:use matrix_calculator.basic_features))
+
+(def determinant [matrix]
+  ("jou"))
---
+++ matrix-calculator/test/matrix_calculator/determinant_test.clj
@@ -0,0 +1,12 @@
+(ns matrix_calculator.determinant-test
+  (:require [clojure.test :refer :all]
+            [matrix_calculator.basic_features :refer :all]
+            [matrix_calculator.helpers :refer :all]))
+
+(deftest multiply.determinant
+  (testing "det"
+    (is (= (determinant [[1 2][2 1]])) -3)
+    (is (= (determinant [[1   2   3]
+                         [3   2   1]
+                         [2   6   4]]) 24.000))
+    (is (= (determinant  [[8    5][11   29]] 177)))))
